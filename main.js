b335ab615c220d640aadaa6d917b0bd92d606f0b
Anıl Tanrıverdiler <186810775+aniltanriverdiler@users.noreply.github.com>
2025-02-28 07:32:15 +0300

    feat: Added increment and decrement functionality to button counters

---
+++ main.js
@@ -0,0 +1,39 @@
+// Buttons Section
+let count = 0;
+
+function updateCounter() {
+   document.getElementById("countDisplay").textContent = count;  
+}
+
+function increase() {
+    count++;
+    updateCounter();
+}
+
+function reset() {
+    count = 0;
+    updateCounter();
+}
+
+
+// Focus Sounds Section
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
